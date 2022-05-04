5a2133daeabb68cc060e0eb27e46cabd6a7d3ccb
leon <abc123@123abcasdasdasdas.com>
2022-05-04 02:38:33 +0200

    nytt test

--- src/main/java/com/example/pvt15app/PersonController.java
+++ src/main/java/com/example/pvt15app/PersonController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @Controller
-@RequestMapping()
+@RequestMapping(path="/app")
 public class PersonController {
     @Autowired
     private PersonRepository personRepository;
