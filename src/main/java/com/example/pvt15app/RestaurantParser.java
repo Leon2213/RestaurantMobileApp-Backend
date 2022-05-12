18522c078f7bc936a2dd84c433f107907e27efe4
leon <abc123@123abcasdasdasdas.com>
2022-05-12 18:45:52 +0200

    nytt test

--- src/main/java/com/example/pvt15app/RestaurantParser.java
+++ src/main/java/com/example/pvt15app/RestaurantParser.java
@@ -13,22 +13,18 @@ import java.util.Optional;
 
 public class RestaurantParser {
     static List<Restaurant> restaurantList = new ArrayList<>();
-    URL url;
-    InputStreamReader reader;
-    JsonParser parser;
-    JsonObject jsonObject;
-    JsonArray jsonArray;
+        URL url;
+        InputStreamReader reader = new InputStreamReader(url.openStream());
+        JsonParser parser = new JsonParser();
+        JsonObject jsonObject = parser.parse(reader).getAsJsonObject();
+        JsonArray jsonArray = jsonObject.get("results").getAsJsonArray();
 
 
 
 
+    public RestaurantParser(double latitude, double longitude) throws IOException {
+        this.url = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=restaurant&location="+latitude+","+longitude+"&radius=500&type=restaurant&key=AIzaSyBmfU0WjQP9e5XMV09t1-UP-M0892jPmkA");
 
-    public RestaurantParser(double longitude, double latitude) throws IOException {
-        url = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=restaurant&location="+latitude+","+longitude+"&radius=500&type=restaurant&key=AIzaSyBmfU0WjQP9e5XMV09t1-UP-M0892jPmkA");
-        this.reader = new InputStreamReader(url.openStream());
-        this.parser = new JsonParser();
-        this.jsonObject = parser.parse(reader).getAsJsonObject();
-        this.jsonArray = jsonObject.get("results").getAsJsonArray();
     }
 
     public void startParsing(){
