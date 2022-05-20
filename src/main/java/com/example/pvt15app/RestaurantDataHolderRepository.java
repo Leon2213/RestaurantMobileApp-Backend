d670bb0fd681190f474f0898f4189759de71afa4
Leon2213 <48647601+Leon2213@users.noreply.github.com>
2022-05-20 16:24:36 +0200

    försökt skriva om klassen från Person till RestaurantDataHolder

--- src/main/java/com/example/pvt15app/RestaurantDataHolderRepository.java
+++ src/main/java/com/example/pvt15app/RestaurantDataHolderRepository.java
@@ -2,8 +2,6 @@ package com.example.pvt15app;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.example.pvt15app.Person;
-
 public interface RestaurantDataHolderRepository extends CrudRepository <RestaurantDataHolder, Integer> {
 
 }
